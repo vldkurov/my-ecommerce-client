3e88b06a89964f7117db7ee598b87511ee400f66
Volodymyr Kurov <vokur13@gmail.com>
2024-03-27 09:16:22 +0000

    Refactor Header component for improved logout handling
    This commit streamlines the Header component and enhances logout functionality. Redundancies and comments were cleaned up to facilitate maintenance. The check for user authentication state has been added which optimizes login/logout option display. Enhanced error handling of logout operation adds resilience to the code.

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,38 +1,31 @@
 import React from 'react';
 import {NavLink as RouterNavLink, useLocation, useNavigate} from 'react-router-dom';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {logout} from '../../../features/auth/authOperations'
 import {StyledAppBar, StyledNavLink, StyledToolbar} from './Header.styled';
 import {Box, Button} from "@mui/material";
-// import api from "../../../api/api";
 
 // import {useAuth} from "../../../hooks/useAuth";
 
 function Header() {
     const location = useLocation();
     const dispatch = useDispatch();
-    // const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
     // const {isAuthenticated} = useAuth();
     const navigate = useNavigate();
 
     const handleLogout = async () => {
-        dispatch(logout());
-
-        // // Making a POST request to the logout endpoint using Axios
-        // await api.post('/users/logout', {}, {
-        //     withCredentials: true // Ensures cookies are sent with the request
-        // })
-        //     .then(response => {
-        //         // Handle response after successful logout
-        //         console.log('Logged out successfully');
-        //         // You might want to redirect the user to the login page or home page here
-        //     })
-        //     .catch(error => {
-        //         // Handle errors, such as network errors or server being unreachable
-        //         console.error('Logout failed', error);
-        //     });
-        //
-        navigate('/');
+        dispatch(logout())
+            .then(response => {
+                // Handle response after successful logout
+                console.log(response.payload);
+                // You might want to redirect the user to the login page or home page here
+                navigate('/');
+            })
+            .catch(error => {
+                // Handle errors, such as network errors or server being unreachable
+                console.error('Logout failed', error);
+            });
     };
 
 
@@ -60,31 +53,20 @@ function Header() {
 
                 {/* Auth segment */}
                 <Box>
-                    {/*{!isAuthenticated ? (*/}
-                    {/*    <>*/}
-                    {/*        <StyledNavLink component={RouterNavLink} to="/register"*/}
-                    {/*                       $isactive={location.pathname === '/register'}>*/}
-                    {/*            Register*/}
-                    {/*        </StyledNavLink>*/}
-                    {/*        <StyledNavLink component={RouterNavLink} to="/login"*/}
-                    {/*                       $isactive={location.pathname === '/login'}>*/}
-                    {/*            Log In*/}
-                    {/*        </StyledNavLink>*/}
-                    {/*    </>*/}
-                    {/*) : (*/}
-                    {/*    <Button color="inherit" onClick={handleLogout}>Log Out</Button>*/}
-                    {/*)}*/}
-                    <>
-                        <StyledNavLink component={RouterNavLink} to="/register"
-                                       $isactive={location.pathname === '/register'}>
-                            Register
-                        </StyledNavLink>
-                        <StyledNavLink component={RouterNavLink} to="/login"
-                                       $isactive={location.pathname === '/login'}>
-                            Log In
-                        </StyledNavLink>
+                    {!isAuthenticated ? (
+                        <>
+                            <StyledNavLink component={RouterNavLink} to="/register"
+                                           $isactive={location.pathname === '/register'}>
+                                Register
+                            </StyledNavLink>
+                            <StyledNavLink component={RouterNavLink} to="/login"
+                                           $isactive={location.pathname === '/login'}>
+                                Log In
+                            </StyledNavLink>
+                        </>
+                    ) : (
                         <Button color="inherit" onClick={handleLogout}>Log Out</Button>
-                    </>
+                    )}
                 </Box>
             </StyledToolbar>
         </StyledAppBar>
