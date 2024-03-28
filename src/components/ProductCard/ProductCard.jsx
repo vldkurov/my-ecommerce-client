4fe6d7d00be10a3c69fd3ec8adadb2ba796bc721
Volodymyr Kurov <vokur13@gmail.com>
2024-03-28 09:36:18 +0000

    Refactor ProductCard component with MUI components
    The ProductCard component has been refactored to use Material-UI's Card. Custom Styled components have been replaced with built-in MUI components, improving consistency and maintenance. Defined structured and better presentation of product details, including price and category, improving the overall user experience.

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,43 +1,82 @@
 import React from 'react';
-import {
-    StyledButton,
-    StyledCard,
-    StyledCardActionArea,
-    StyledCardActions,
-    StyledCardContent,
-    StyledCardMedia,
-    StyledTypographyDescription,
-    StyledTypographyTitle,
-} from './ProductCard.styled';
-import {Link} from "react-router-dom";
+import {Card, CardContent, CardMedia, Typography} from '@mui/material';
+import {Link} from 'react-router-dom';
+import {StyledButton, StyledCardActions} from './ProductCard.styled';
 
 const ProductCard = ({product}) => {
+    const {
+        imageUrl,
+        name,
+        description,
+        price,
+        category
+    } = product;
     const placeholderImage = '/path-to-default-image.jpg';
 
     return (
-        <StyledCard>
-            <StyledCardActionArea to={`/products/${product.productId}`}>
-                <StyledCardMedia
-                    component="img"
-                    image={product.imageUrl || placeholderImage}
-                    alt={product.name}
-                />
-                <StyledCardContent>
-                    <StyledTypographyTitle variant="h5" component="div" noWrap>
-                        {product.name}
-                    </StyledTypographyTitle>
-                    <StyledTypographyDescription variant="body2" color="text.secondary">
-                        {product.description}
-                    </StyledTypographyDescription>
-                </StyledCardContent>
-            </StyledCardActionArea>
+        <Card sx={{height: '100%', display: 'flex', flexDirection: 'column'}}>
+            <CardMedia
+                component="img"
+                image={imageUrl || placeholderImage}
+                alt={name}
+                sx={{height: 140, objectFit: 'cover'}}
+            />
+            <CardContent sx={{
+                flexGrow: 1,
+                display: 'flex',
+                flexDirection: 'column',
+            }}>
+                <Typography gutterBottom variant="h5" component="div" sx={{
+                    whiteSpace: 'nowrap',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    width: '100%', // Ensures it doesn't exceed the card width
+                }}>
+                    {name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary" sx={{
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    display: '-webkit-box',
+                    WebkitLineClamp: 3, // Limit text to 3 lines
+                    WebkitBoxOrient: 'vertical',
+                    height: '64px', // Fixed height for 3 lines of text
+                    mb: 2
+                }}>
+                    {description}
+                </Typography>
+                <Typography variant="body2" color="text.primary" sx={{mb: 1}}>
+                    Price: {price}
+                </Typography>
+                <Typography variant="subtitle1" color="text.primary" sx={{mb: 1}}>
+                    Category: {category?.name || 'No Category'}
+                </Typography>
+                <Typography variant="body2" color="text.secondary" sx={{
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    display: '-webkit-box',
+                    WebkitLineClamp: 2, // Adjust based on your needs
+                    WebkitBoxOrient: 'vertical',
+                    height: '38px', // Fixed height for 2 lines of text
+                }}>
+                    {category?.description || 'No Description'}
+                </Typography>
+            </CardContent>
             <StyledCardActions>
                 <StyledButton size="small" component={Link} to={`/products/${product.productId}`}>
                     View Details
                 </StyledButton>
             </StyledCardActions>
-        </StyledCard>
+        </Card>
     );
 };
 
 export default ProductCard;
+
+
+
+
+
+
+
+
