1c51617cf1c6503f2628d87bef152eebe15a7be8
Volodymyr Kurov <vokur13@gmail.com>
2024-04-25 10:37:31 +0100

    Refactor user status check and update Axios settings
    Modified the `check` function in `authOperations.js` to handle specific error and authentication cases more appropriately. Enhanced error handling to deal with a 401 response specifically, signaling a session expiration. Also, moved `withCredentials` setting from Axios defaults to its instance creation for better clarity and organization.

--- src/api/api.js
+++ src/api/api.js
@@ -6,8 +6,9 @@ const baseURL = process.env.REACT_APP_BASE_URL
 // Create an Axios instance with pre-configured settings
 const api = axios.create({
     baseURL: baseURL,
+    withCredentials: true  // This should be set if you are using session cookies
 });
 
-api.defaults.withCredentials = true;
+// api.defaults.withCredentials = true;
 
 export default api;
